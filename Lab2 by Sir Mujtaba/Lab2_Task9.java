cb7f0b14cfb198b56a9444247ac1bd214a864a86
muhammad-zaid1507 <hafizzaid768@gmail.com>
2026-05-25 11:38:52 +0500

    Add files via upload

---
+++ Lab2 by Sir Mujtaba/Lab2_Task9.java
@@ -0,0 +1,10 @@
+public class Lab2_Task9 {
+    public static void main(String[] args){
+        int a1=10 , a2= 20;
+        if(a1!=a2)
+        {
+            System.out.println(a1 + " Is not EQUALS to " + a2);
+        }
+        System.out.println(++a1);
+    }
+}
