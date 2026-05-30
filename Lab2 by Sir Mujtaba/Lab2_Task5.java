3ef0f5f12ca54da8770022fb9d73f0a3326ce179
muhammad-zaid1507 <hafizzaid768@gmail.com>
2026-05-31 03:06:26 +0500

    Delete Lab2 by Sir Mujtaba directory

--- Lab2 by Sir Mujtaba/Lab2_Task5.java
+++
@@ -1,8 +0,0 @@
-public class Lab2_Task5 {
-    public static void main(String[] args) {
-
-        int randomNumber = (int) (Math.random() * 11);
-        int userGuess = 5;
-        System.out.println(randomNumber == userGuess);
-    }
-}
--- Lab2 by Sir Mujtaba/Lab2_Task6.java
+++
@@ -1,8 +0,0 @@
-public class Lab2_Task6 {
-    public static void main(String[] args){
-        String first_name= "Muhammad";
-        String last_name= "Zaid";
-        System.out.println(first_name + " " + last_name + " ");
-        System.out.println(first_name.concat(" " + last_name));
-    }
-}
